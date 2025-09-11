fcab32e54a5287722ed5b4f77342e987a756c781
Jestin VanScoyoc <31118852+JediScoy@users.noreply.github.com>
2025-09-11 15:18:05 -0400

    Update DriveSubsystem.java

--- TankBot/src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ TankBot/src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -7,23 +7,23 @@ import frc.robot.Constants;
 public class DriveSubsystem extends SubsystemBase {
     private final TalonFX leftLead = new TalonFX(Constants.Drive.LEFT_PRIMARY_ID);
     private final TalonFX leftFollow = new TalonFX(Constants.Drive.LEFT_SECONDARY_ID);
-    // private final TalonFX rightLead = new TalonFX(Constants.Drive.RIGHT_PRIMARY_ID);
-    // private final TalonFX rightFollow = new TalonFX(Constants.Drive.RIGHT_SECONDARY_ID);
+    private final TalonFX rightLead = new TalonFX(Constants.Drive.RIGHT_PRIMARY_ID);
+    private final TalonFX rightFollow = new TalonFX(Constants.Drive.RIGHT_SECONDARY_ID);
 
     public DriveSubsystem() {
         leftFollow.setControl(Constants.Drive.FOLLOW(leftLead));
-        // rightFollow.setControl(Constants.Drive.FOLLOW(rightLead));
+        rightFollow.setControl(Constants.Drive.FOLLOW(rightLead));
     }
 
     public void arcadeDrive(double forward, double rotation) {
         double leftOutput = forward + rotation;
-        // double rightOutput = forward - rotation;
+        double rightOutput = forward - rotation;
         leftLead.set(leftOutput);
-        // rightLead.set(rightOutput);
+        rightLead.set(rightOutput);
     }
 
     public void stop() {
         leftLead.set(0);
-        // rightLead.set(0);
+        rightLead.set(0);
     }
 }
